b946d058d0435f7eb01333b49823e4d70c2588e9
Ekky Hadits Firdaus <129919257+SpaceMan586@users.noreply.github.com>
2026-02-10 08:30:39 +0700

    Fix mobile navbar toggle visibility

--- FrontEnd/app/(WebsiteUtama)/components/LandingNavbar.tsx
+++ FrontEnd/app/(WebsiteUtama)/components/LandingNavbar.tsx
@@ -49,7 +49,7 @@ export function LandingNavbar() {
       </NavbarBrand>
 
       {/* TOGGLE (MOBILE) */}
-      <NavbarToggle className="md:hidden text-black focus:ring-0 ml-auto [&>span]:hidden" />
+      <NavbarToggle className="md:hidden text-black focus:ring-0 ml-auto" />
 
       {/* DESKTOP MENU (RIGHT ALIGNED) */}
       <div className="hidden md:flex flex-1 justify-end items-center gap-16 lg:gap-10">
